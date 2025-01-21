fcd9dd5168681bbe7110123a158bae6e651258b8
Wouter Klaassen <wr.klaassen@student.avans.nl>
2025-01-21 16:10:33 +0100

    newernewestcode

--- multi_stepper_sub_endswitch_reconnector/multi_stepper_sub_endswitch_reconnector.ino
+++ multi_stepper_sub_endswitch_reconnector/multi_stepper_sub_endswitch_reconnector.ino
@@ -9,11 +9,19 @@
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
+#include <rmw_microros/rmw_microros.h>
 
 #include <std_msgs/msg/int32.h>
 #include <std_msgs/msg/int8.h>
 
 #include <geometry_msgs/msg/point32.h>
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return false;}}
+#define EXECUTE_EVERY_N_MS(MS, X)  do { \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+} while (0)\
 
 rcl_subscription_t servo_subscriber;
 std_msgs__msg__Int32 servo_msg;
@@ -27,6 +35,14 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 rcl_timer_t timer;
 
+bool micro_ros_init_successful;
+
+enum states {
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+} state;
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
@@ -143,7 +159,14 @@ void servo_callback(const void * msgin)
   gripperservo.write(servo_position);
 }
 
-
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  (void) last_call_time;
+  if (timer != NULL) {
+    rcl_publish(&publisher, &msg, NULL);
+    msg.data++;
+  }
+}
 
 void xyz_callback(const void * msgin)
 {
@@ -164,13 +187,8 @@ void error_loop(){
   }
 }
 
-void setup() {
-  // Seriële monitor
-  Serial.begin(115200);
-
-  // Microros setup
-  set_microros_transports();
-
+bool create_entities()
+{
   allocator = rcl_get_default_allocator();
 
   // create init_options
@@ -194,11 +212,48 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Point32),
       "/xyz"));
 
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
 
   // create executor
+  executor = rclc_executor_get_zero_initialized_executor();
   RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
   RCCHECK(rclc_executor_add_subscription(&executor, &servo_subscriber, &servo_msg, &servo_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &xyz_subscriber, &xyz_msg, &xyz_callback, ON_NEW_DATA));
+
+
+  return true;
+}
+
+void destroy_entities()
+{
+  rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+  (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+  rcl_publisher_fini(&publisher, &node);
+  rcl_timer_fini(&timer);
+  rclc_executor_fini(&executor);
+  rcl_node_fini(&node);
+  rclc_support_fini(&support);
+}
+
+void setup() {
+  // Seriële monitor
+  Serial.begin(115200);
+
+  // Microros setup
+  set_microros_transports();
+  pinMode(LED_PIN, OUTPUT);
+
+  state = WAITING_AGENT;
+
+  msg.data = 0;
 
   // Motor setup
   motorSetup();
@@ -225,7 +280,35 @@ void Task1code( void * pvParameters){
   Serial.print("Task1 Running on Core ");
   Serial.println(xPortGetCoreID());
   for(;;){
-    RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+    switch (state) {
+      case WAITING_AGENT:
+        EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
+        break;
+      case AGENT_AVAILABLE:
+        state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
+        if (state == WAITING_AGENT) {
+          destroy_entities();
+        };
+        break;
+      case AGENT_CONNECTED:
+        EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
+        if (state == AGENT_CONNECTED) {
+          rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+        }
+        break;
+      case AGENT_DISCONNECTED:
+        destroy_entities();
+        state = WAITING_AGENT;
+        break;
+      default:
+        break;
+    }
+
+    if (state == AGENT_CONNECTED) {
+      Serial.println("AGENT CONNECTED");
+    } else {
+      Serial.println("NO AGENT CONNECTED");
+    }
   }
 }
 
